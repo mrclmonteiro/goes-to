94104f37e8f0f8ff7c5789f15e88fb157b2633ad
Marcelo Monteiro <109428220+mrclmonteiro@users.noreply.github.com>
2026-03-12 18:18:16 -0300

    ajustes nas metadatas do aplicativo

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,12 +34,12 @@ export const metadata: Metadata = {
     description: "Um novo app para acompanhar, avaliar e compartilhar os filmes indicados às principais premiações",
     images: [{ url: "/og-image.png", width: 1200, height: 630 }],
   },
-    keywords: {
+    keywords: [
     "votar oscar",
     "filmes oscar",
     "letterboxd oscar",
     "aplicativo oscar"
-    },
+  ],
   twitter: {
     card: "summary_large_image",
     images: ["/og-image.png"],
